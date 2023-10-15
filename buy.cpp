2213265633d6721bbf1cf2c431b1ecdd6e33ea2a
Vinh Nguyen <vinhgrh3@gmail.com>
2023-10-15 07:58:39 +0700

    change

--- buy.cpp
+++ buy.cpp
@@ -1,7 +1,8 @@
 #include "buy.h"
-void Buy::Purchase() {
-    cout << "Moi nhap ma sp  : " ;
-    string idProduct ; 
+void Buy::Purchase()
+{
+    cout << "Moi nhap ma sp  : ";
+    string idProduct;
     cin >> idProduct;
-    
+    cout << "ok";
 }
